3cd042596b6e8bc7234d5492d444ce73ca6235ec
Bikram Upadhyay <upadhyaybikram@gmail.com>
2023-12-08 09:40:57 +1100

    #dependencies imported for using state and axiox #register and get api key from weather api #onclick pass fetchweather

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,14 @@
-// ... (imports)
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState(null);
 
-  const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+  const apiKey = 'ff3b3aba3af42f3a338094469c8cd600'; //this is just a demo, for multiple keys,please store on .env file 
+  console.log('open weather API',apiKey)
 
   const fetchWeather = async () => {
     try {
@@ -18,5 +21,29 @@ function App() {
     }
   };
 
-  // ... (rest of the component)
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={fetchWeather}>Get Weather</button>
+      </div>
+      {weather && (
+        <div>
+          <h2>{weather.name}, {weather.sys.country}</h2>
+          <p>{weather.weather[0].description}</p>
+          <p>Temperature: {weather.main.temp}°C</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+        </div>
+      )}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+  );
 }
+
+export default App;
